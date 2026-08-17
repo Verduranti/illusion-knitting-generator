b2a1dc5f931a682630df9f4715989db37ac86764
Chris <angelofchaos@gmail.com>
2026-08-17 14:05:51 -0700

    Namespace Astro assets for proxy deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://illusion-knitting-generator.vercel.app',
   output: 'static',
+  build: {
+    assets: 'shadowknits-assets',
+  },
 });
